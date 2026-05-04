d8577694b3fd80b444cf9ab53fdc23e4b0a8e147
chagi666 <37004688+chagi666@users.noreply.github.com>
2026-05-04 12:51:31 -0500

    Fix CORS headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,6 @@ const categoriasRoutes = require('./routes/categorias');
 
 const app = express();
 
-// Headers CORS manuales
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
